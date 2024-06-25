638435583fc371006804df91bc65b692d81e25d1
Bahrom Parpiyev <bahrom1982@gmail.com>
2024-06-25 10:34:46 +0500

    Commit

--- Modules/Investors/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('investors::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('investors.name') !!}
-    </p>
-@endsection
